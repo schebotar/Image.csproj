413bb386f9b8b447570a443a45f2fa3b7898a2ee
Sergey Chebotar <s.chebotar@gmail.com>
2020-01-16 20:18:19 +0300

    Решение задачи Медианный фильтр

--- MedianFilterTask.cs
+++ MedianFilterTask.cs
@@ -1,21 +1,73 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Recognizer
 {
 	internal static class MedianFilterTask
 	{
-		/* 
-		 * Для борьбы с пиксельным шумом, подобным тому, что на изображении,
-		 * обычно применяют медианный фильтр, в котором цвет каждого пикселя, 
-		 * заменяется на медиану всех цветов в некоторой окрестности пикселя.
-		 * https://en.wikipedia.org/wiki/Median_filter
-		 * 
-		 * Используйте окно размером 3х3 для не граничных пикселей,
-		 * Окно размером 2х2 для угловых и 3х2 или 2х3 для граничных.
-		 */
+		public static double[,] GetFramedImage(double[,] notFramed)
+		{
+			var xLength = notFramed.GetLength(0);
+			var yLength = notFramed.GetLength(1);
+			var framedOriginal = new double[xLength + 2, yLength + 2];			
+
+			for (int x = 0; x < xLength + 2; x++)
+			{
+				for (int y = 0; y < yLength + 2; y++)
+					framedOriginal[x, y] = -1;
+			}
+
+			for (int x = 0; x < xLength; x++)
+			{
+				for (int y = 0; y < yLength; y++)
+					framedOriginal[x + 1, y + 1] = notFramed[x, y];
+			}
+			return framedOriginal;
+		}
+
+		public static List<double> GetListOfValues(int x, int y, double[,] framedOriginal)
+		{
+			var pixelValues = new List<double>();
+
+			for (int i = -1; i < 2; i++)
+			{
+				for (int j = -1; j < 2; j++)
+				{
+					if (framedOriginal[x + i, y + j] != -1)
+						pixelValues.Add(framedOriginal[x + i, y + j]);
+				}				
+			}
+			return pixelValues;
+		}
+
+		public static double GetMedian(List<double> pixelValues)
+		{
+			double median = 0.0;
+			pixelValues.Sort();
+			if (pixelValues.Count % 2 == 1)
+				median = pixelValues[(pixelValues.Count + 1) / 2 - 1];
+			else
+				median = (pixelValues[pixelValues.Count / 2 - 1] + pixelValues[pixelValues.Count / 2]) / 2;			
+			return median;
+		}
+
 		public static double[,] MedianFilter(double[,] original)
 		{
-			return original;
+			var framedOriginal = GetFramedImage(original);
+			var framedXLength = framedOriginal.GetLength(0);
+			var framedYLength = framedOriginal.GetLength(1);
+			var result = new double[original.GetLength(0), original.GetLength(1)];
+
+			for (int x = 1; x < framedXLength - 1; x++)
+			{
+				for (int y = 1; y < framedYLength - 1; y++)
+				{
+					var pixelValues = GetListOfValues(x, y, framedOriginal);
+					var median = GetMedian(pixelValues);
+					result[x - 1, y - 1] = median;
+				}
+			}
+			return result;
 		}
 	}
 }
---
+++ MedianFilterTaskTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Recognizer
+{
+    [TestFixture]
+    public class MedianFilterTaskTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(new double[,] { { 0.5 } }, MedianFilterTask.MedianFilter(new double[,] { {0.5} }));
+        }
+
+
+    }
+}
