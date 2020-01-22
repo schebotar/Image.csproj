cc5020e1d9182b7ba449c65544e99f44dddad459
Sergey Chebotar <s.chebotar@gmail.com>
2020-01-22 19:44:12 +0300

    Решение ThresholdFilter

--- ThresholdFilterTask.cs
+++ ThresholdFilterTask.cs
@@ -1,10 +1,50 @@
-﻿namespace Recognizer
+﻿using System.Collections.Generic;
+
+namespace Recognizer
 {
 	public static class ThresholdFilterTask
 	{
+		public static double GetThreshold(double[,] original, double whitePixelsFraction)
+		{
+			double thresholdT = 0.0;
+			var pixelsCount = original.Length;
+
+			var pixels = new List<double>();
+
+			foreach (var e in original)
+				pixels.Add(e);
+
+			pixels.Sort();
+			
+			var whitePixelsCount = (int)(whitePixelsFraction * pixelsCount);
+			if (whitePixelsCount > 0 && whitePixelsCount <= pixelsCount)
+				thresholdT = pixels[pixelsCount - whitePixelsCount];
+			else
+				thresholdT = int.MaxValue;
+
+			return thresholdT;
+		}
+
+
 		public static double[,] ThresholdFilter(double[,] original, double whitePixelsFraction)
 		{
-			return new double[original.GetLength(0), original.GetLength(1)];
+			var thresholdT = GetThreshold(original, whitePixelsFraction);
+
+			var originalX = original.GetLength(0);
+			var originalY = original.GetLength(1);
+
+			for (int x = 0; x < originalX; x++)
+			{
+				for (int y = 0; y < originalY; y++)
+				{
+					if (original[x, y] >= thresholdT)
+						original[x, y] = 1.0;
+					else
+						original[x, y] = 0.0;
+				}
+			}
+
+			return original;
 		}
 	}
 }
---
+++ ThresholdFilterTaskTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Recognizer
+{
+    [TestFixture]
+    public class ThresholdFilterTaskTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(new double[,] { { 0 } }, ThresholdFilterTask.ThresholdFilter(new double[,] { { 123 } }, 0));
+        }
+            
+    }
+}
